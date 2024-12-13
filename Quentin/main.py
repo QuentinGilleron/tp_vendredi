50e967a63d2cffb76fedfe740f8e44b7751e5c7a
Quentin Gilleron <quentin.gilleron5627@gmail.com>
2024-12-13 13:36:32 +0100

    update menu

--- Quentin/main.py
+++ Quentin/main.py
@@ -16,20 +16,24 @@ def menu_combat():
     print("| 3. Fuir                                  |")
     print("  ========================================  ")
 
-def creer_personnage(nom, classe, niveau, points_de_vie,):
+def space():
+    for i in range(3):
+        print("")
+
+def creer_personnage(nom, classe, niveau):
     return {
         "nom": nom,
         "classe": classe,
         "niveau": niveau,
-        "points_de_vie": points_de_vie,
+        "points_de_vie": 100,
         "inventaire": [
             {"nom": "potion de soin", "quantité": 3},
             {"nom": "épée", "quantité": 1}
         ]
     }
 
-personnage1 = creer_personnage("Quentin", "Guerrier", 1, 100)
-personnage2 = creer_personnage("Ennemi", "Guerrier", 1, 100)
+personnage1 = creer_personnage("Quentin", "Guerrier", 1)
+personnage2 = creer_personnage("Ennemi", "Guerrier", 1)
 
 def ajouter_objet(personnage, objet):
     personnage["inventaire"].append(objet)
@@ -44,8 +48,11 @@ def utiliser_potion_de_soin(personnage):
             if objet["quantité"] == 0:
                 personnage["inventaire"].remove(objet)
             break
-    print("Vous avez utilisé une potion de soin")
-    print(f"Vous avez gagé {gain} points de vie")
+    print("| Glou glou glou ...")
+    print("|")
+    print(f"| Vous avez gagné {gain} points de vie")
+    print("|")
+    print(f"| Vous avez désormais {personnage['points_de_vie']} points de vie")
     return personnage
 
 def informations_personnage(personnage):
@@ -60,53 +67,92 @@ def afficher_inventaire(personnage):
 
 def combat(personnage1, personnage2):
     # menu de combat, avec les choix d'attaque, de potion, de fuite
-    
-    print("============== COMBAT ================")
-    print(f"{personnage1['nom']} VS {personnage2['nom']}")
-    menu_combat()
-    choix = input("Que voulez-vous faire ? ")
-    while choix != "3":
-        if choix == "1":
-            
-            print(" ======================================================================================== ")
-            degats = randint(1,10) * personnage1["niveau"]
-            personnage2["points_de_vie"] -= degats
+    if personnage2["points_de_vie"] <= 0:
+        print(" ======================================== ")
+        print("|        Vous avez déjà gagné !          |")
+        print("|░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░|")
+        print("|░░░░░░░▄▄▀▀▀▀▀▀▀▀▀▀▄▄█▄░░░░▄░░░░█░░░░░░░|")
+        print("|░░░░░░█▀░░░░░░░░░░░░░▀▀█▄░░░▀░░░░░░░░░▄░|")
+        print("|░░░░▄▀░░░░░░░░░░░░░░░░░▀██░░░▄▀▀▀▄▄░░▀░░|")
+        print("|░░▄█▀▄█▀▀▀▀▄░░░░░░▄▀▀█▄░▀█▄░░█▄░░░▀█░░░░|")
+        print("|░▄█░▄▀░░▄▄▄░█░░░▄▀▄█▄░▀█░░█▄░░▀█░░░░█░░░|")
+        print("|▄█░░█░░░▀▀▀░█░░▄█░▀▀▀░░█░░░█▄░░█░░░░█░░░|")
+        print("|██░░░▀▄░░░▄█▀░░░▀▄▄▄▄▄█▀░░░▀█░░█▄░░░█░░░|")
+        print("|██░░░░░▀▀▀░░░░░░░░░░░░░░░░░░█░▄█░░░░█░░░|")
+        print("|██░░░░░░░░░░░░░░░░░░░░░█░░░░██▀░░░░█▄░░░|")
+        print("|██░░░░░░░░░░░░░░░░░░░░░█░░░░█░░░░░░░▀▀█▄|")
+        print("|██░░░░░░░░░░░░░░░░░░░░█░░░░░█░░░░░░░▄▄██|")
+        print("|░██░░░░░░░░░░░░░░░░░░▄▀░░░░░█░░░░░░░▀▀█▄|")
+        print("|░▀█░░░░░░█░░░░░░░░░▄█▀░░░░░░█░░░░░░░▄▄██|")
+        print("|░▄██▄░░░░░▀▀▀▄▄▄▄▀▀░░░░░░░░░█░░░░░░░▀▀█▄|")
+        print("|░░▀▀▀▀░░░░░░░░░░░░░░░░░░░░░░█▄▄▄▄▄▄▄▄▄██|")
+        print("|░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░|")
+        print(" ======================================== ")
 
-            print(f"{personnage1['nom']} attaque {personnage2['nom']} et lui inflige {degats} points de dégats")
-
-            if personnage2["points_de_vie"] <= 0:
-                personnage1["inventaire"].extend(personnage2["inventaire"])
-                personnage2["inventaire"] = []
-                personnage1["niveau"] += 1
-                print(" ===================================== ")
-                print("|          Vous avez gagné !          |")
-                print(" ===================================== ")
-                break
-
-            degats = randint(1,10) * personnage2["niveau"]
-            personnage1["points_de_vie"] -= degats
-
-            print(f"{personnage2['nom']} attaque {personnage1['nom']} et lui inflige {degats} points de dégats")	
-
-            if personnage1["points_de_vie"] <= 0:
-                print(" ===================================== ")
-                print("|          Vous avez perdu !          |")
-                print(" ===================================== ")
-                exit()
-
-            print(" ================ STAT PV ===================== ")
-            print(f"| {personnage1['nom']} : {personnage1['points_de_vie']} PV")
-            print(f"| {personnage2['nom']} : {personnage2['points_de_vie']} PV")
-            print(" =============================================== ")
-
-        elif choix == "2":
-            personnage1 = utiliser_potion_de_soin(personnage1)
-        else:
-            print(" ===================================== ")
-            print("|           Choix invalide !          |")
-            print(" ===================================== ")
+    else:
+        print("============== COMBAT ================")
+        print(f"{personnage1['nom']} VS {personnage2['nom']}")
         menu_combat()
         choix = input("Que voulez-vous faire ? ")
+        while choix != "3":
+            if choix == "1":
+                space()
+                print(" ======================================================================================== ")
+                degats = randint(1,10) * personnage1["niveau"]
+                personnage2["points_de_vie"] -= degats
+
+                print(f"{personnage1['nom']} attaque {personnage2['nom']} et lui inflige {degats} points de dégats")
+
+                if personnage2["points_de_vie"] <= 0:
+                    personnage1["inventaire"].extend(personnage2["inventaire"])
+                    personnage2["inventaire"] = []
+                    personnage1["niveau"] += 1
+                    print(" ======================================== ")
+                    print("|            Vous avez gagné !           |")
+                    print("|░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░|")
+                    print("|░░░░░░░▄▄▀▀▀▀▀▀▀▀▀▀▄▄█▄░░░░▄░░░░█░░░░░░░|")
+                    print("|░░░░░░█▀░░░░░░░░░░░░░▀▀█▄░░░▀░░░░░░░░░▄░|")
+                    print("|░░░░▄▀░░░░░░░░░░░░░░░░░▀██░░░▄▀▀▀▄▄░░▀░░|")
+                    print("|░░▄█▀▄█▀▀▀▀▄░░░░░░▄▀▀█▄░▀█▄░░█▄░░░▀█░░░░|")
+                    print("|░▄█░▄▀░░▄▄▄░█░░░▄▀▄█▄░▀█░░█▄░░▀█░░░░█░░░|")
+                    print("|▄█░░█░░░▀▀▀░█░░▄█░▀▀▀░░█░░░█▄░░█░░░░█░░░|")
+                    print("|██░░░▀▄░░░▄█▀░░░▀▄▄▄▄▄█▀░░░▀█░░█▄░░░█░░░|")
+                    print("|██░░░░░▀▀▀░░░░░░░░░░░░░░░░░░█░▄█░░░░█░░░|")
+                    print("|██░░░░░░░░░░░░░░░░░░░░░█░░░░██▀░░░░█▄░░░|")
+                    print("|██░░░░░░░░░░░░░░░░░░░░░█░░░░█░░░░░░░▀▀█▄|")
+                    print("|██░░░░░░░░░░░░░░░░░░░░█░░░░░█░░░░░░░▄▄██|")
+                    print("|░██░░░░░░░░░░░░░░░░░░▄▀░░░░░█░░░░░░░▀▀█▄|")
+                    print("|░▀█░░░░░░█░░░░░░░░░▄█▀░░░░░░█░░░░░░░▄▄██|")
+                    print("|░▄██▄░░░░░▀▀▀▄▄▄▄▀▀░░░░░░░░░█░░░░░░░▀▀█▄|")
+                    print("|░░▀▀▀▀░░░░░░░░░░░░░░░░░░░░░░█▄▄▄▄▄▄▄▄▄██|")
+                    print("|░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░|")
+                    print(" ======================================== ")
+                    break
+
+                degats = randint(1,10) * personnage2["niveau"]
+                personnage1["points_de_vie"] -= degats
+
+                print(f"{personnage2['nom']} attaque {personnage1['nom']} et lui inflige {degats} points de dégats")	
+
+                if personnage1["points_de_vie"] <= 0:
+                    print(" ===================================== ")
+                    print("|          Vous avez perdu !          |")
+                    print(" ===================================== ")
+                    exit()
+
+                print(" ================ STAT PV ===================== ")
+                print(f"| {personnage1['nom']} : {personnage1['points_de_vie']} PV")
+                print(f"| {personnage2['nom']} : {personnage2['points_de_vie']} PV")
+                print(" =============================================== ")
+
+            elif choix == "2":
+                personnage1 = utiliser_potion_de_soin(personnage1)
+            else:
+                print(" ===================================== ")
+                print("|           Choix invalide !          |")
+                print(" ===================================== ")
+            menu_combat()
+            choix = input("Que voulez-vous faire ? ")
 
 if __name__ == "__main__":
 
@@ -116,22 +162,27 @@ if __name__ == "__main__":
     while choix != "5":
 
         if choix == "1":
+            
+            space()
             print(" =======================================")
             informations_personnage(personnage1)
             print(" =======================================")
         elif choix == "2":
+            space()
             print("========================================")
             afficher_inventaire(personnage1)
             print("========================================")
         elif choix == "3":
+            space()
             print("========================================")
             personnage1 = utiliser_potion_de_soin(personnage1)
             print("========================================")
         elif choix == "4":
-            print("========================================")
+            space()
             combat(personnage1, personnage2)
-            print("========================================")
+            space()
         else:
+            space()
             print(" ====================================== ")
             print("|           Choix invalide !           |")
             print(" ====================================== ")
